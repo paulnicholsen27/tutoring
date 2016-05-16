8e8b04e5364fc483e44ce583f9e3383fffaa08b9
Paul Nicholsen <pnichols104@gmail.com>
2016-05-15 21:23:48 -0400

    using ckeditor for blog posts

--- mainsite/models.py
+++ mainsite/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.urlresolvers import reverse
 
+from ckeditor.fields import RichTextField
+
 class Blog(models.Model):
 
     PUBLISH_CHOICES = ((1, "Published"),
@@ -11,8 +13,8 @@ class Blog(models.Model):
 
     title = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    opening_content = models.TextField()
-    extended_content = models.TextField(blank=True, null=True)
+    opening_content = RichTextField()
+    extended_content = RichTextField(blank=True, null=True)
     published = models.IntegerField(choices=PUBLISH_CHOICES, default=2)
     publish_date = models.DateField(db_index=True, auto_now_add=True)
 
--- withinreach/settings.py
+++ withinreach/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainsite',
+    'ckeditor',
 ]
 
 MIDDLEWARE_CLASSES = [
