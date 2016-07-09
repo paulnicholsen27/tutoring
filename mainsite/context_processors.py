87dc2878b9007c48ac960b941743aaa196fd11f4
Paul Nicholsen <pnichols104@gmail.com>
2016-07-09 09:24:13 -0400

    added google maps

---
+++ mainsite/context_processors.py
@@ -0,0 +1,5 @@
+from common.tokens_and_keys import GOOGLE_API_KEY
+
+
+def google_api(request):
+    return {"google_api_key": GOOGLE_API_KEY}
--- mainsite/views.py
+++ mainsite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, FormView
 
 from forms import ContactForm
-
+from common.tokens_and_keys import GOOGLE_API_KEY
 
 class HomepageView(TemplateView):
     template_name = "homepage.html"
@@ -14,11 +14,11 @@ class AboutView(TemplateView):
 
 class ContactView(FormView):
     template_name = "contact.html"
-
     form_class = ContactForm
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial=self.initial)
+        print "GET"
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
@@ -28,9 +28,10 @@ class ContactView(FormView):
             return redirect('contact')
         return render(request, self.template_name, {'form': form})
 
-    def get_context_data(self, **kwargs):
-        context = super(ContactView, self).get_context_data(**kwargs)
-        context.update({"form": self.form_class})
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super(ContactView, self).get_context_data(**kwargs)
+    #     print GOOGLE_API_KEY
+    #     context.update({"form": self.form_class, "google_api": GOOGLE_API_KEY})
+    #     return context
 
 
