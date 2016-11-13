d35b608d874e1818977902c811909af3b640ac9d
Paul Nicholsen <pnichols104@gmail.com>
2016-11-13 11:35:37 -0500

    took slug off admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 
 from .models import Blog
 
+
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    
+
     prepopulated_fields = {'slug': ('title',)}
-    list_display =("title", "published", "publish_date")
+    list_display = ("title", "published", "publish_date")
+    fields = ("title", "opening_content", "extended_content", "published")
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            self.prepopulated_fields = {}
+            return self.readonly_fields + ('slug',)
+        return self.readonly_fields
