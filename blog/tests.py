38a4f740ea8e1cc6690ce41067cbc073a0d30c1e
Paul Nicholsen <pnichols104@gmail.com>
2016-11-14 21:38:59 -0500

    added tests for next/prev entry on blog

--- blog/tests.py
+++ blog/tests.py
@@ -68,6 +68,14 @@ class TestPublishedMethods(TestCase):
         )
         self.unpublished_blog.save()
 
+        self.another_published_blog = Blog(
+            title="also published",
+            opening_content="opening content",
+            extended_content="extended content",
+            published=1
+        )
+        self.another_published_blog.save()
+
     def test_blog_main_load(self):
         blog_main_url = reverse("blog:blog")
         r = self.client.get(blog_main_url)
@@ -80,3 +88,17 @@ class TestPublishedMethods(TestCase):
                                           "pk": self.unpublished_blog.pk})
         r = self.client.get(blog_detail_url)
         self.assertEqual(r.status_code, 404)
+
+    def test_blog_next(self):
+        blog_detail_url = reverse("blog:blog_detail",
+                                  kwargs={"slug": self.published_blog.slug,
+                                          "pk": self.published_blog.pk})
+        response = self.client.get(blog_detail_url)
+        self.assertEqual(response.context["next_entry"], self.another_published_blog)
+
+    def test_blog_prev(self):
+        blog_detail_url = reverse("blog:blog_detail",
+                                  kwargs={"slug": self.another_published_blog.slug,
+                                          "pk": self.another_published_blog.pk})
+        response = self.client.get(blog_detail_url)
+        self.assertEqual(response.context["prev_entry"], self.published_blog)
