7bc5c10fa9eaabdd790a173cb6d75896e4fb8e45
Paul Nicholsen <pnichols104@gmail.com>
2016-11-20 11:43:06 -0500

    removed slug from prepopulated fields

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,6 @@ from .models import Blog
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
 
-    prepopulated_fields = {'slug': ('title',)}
     list_display = ("title", "published", "publish_date")
     fields = ("title", "opening_content", "extended_content", "published")
     def get_readonly_fields(self, request, obj=None):
