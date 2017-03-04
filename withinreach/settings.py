4d4e216d22ebfd5c50802d15e906291936fc52b8
Paul Nicholsen <paulnicholsen27@gmail.com>
2017-03-04 11:16:48 -0500

    updated google analytics

--- withinreach/settings.py
+++ withinreach/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.withinreachgroup.com', ".fathomless-harbor-44165.herokuapp.com", ".herokuapp.com"]
 
